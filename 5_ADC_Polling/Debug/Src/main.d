83fc4a341a4515d67235494fdd184f2068aeda3d
Amrita <amritans2017@gmail.com>
2024-05-03 20:11:33 +0530

    added

---
+++ 5_ADC_Polling/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/CMSIS/5_ADC_Polling/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/CMSIS/5_ADC_Polling/Core/Include/core_cm4.h \
+ C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_version.h \
+ C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_compiler.h \
+ C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_gcc.h \
+ C:/CMSIS/5_ADC_Polling/Core/Include/mpu_armv7.h \
+ C:/CMSIS/5_ADC_Polling/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/CMSIS/5_ADC_Polling/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+C:/CMSIS/5_ADC_Polling/Core/Include/core_cm4.h:
+C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_version.h:
+C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_compiler.h:
+C:/CMSIS/5_ADC_Polling/Core/Include/cmsis_gcc.h:
+C:/CMSIS/5_ADC_Polling/Core/Include/mpu_armv7.h:
+C:/CMSIS/5_ADC_Polling/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
